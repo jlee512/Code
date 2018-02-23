13fefb5283d5276c20a8184a66b55266f24f9f99
Julian Lees <julian.j.lees@gmail.com>
2018-02-24 00:19:16 +1300

    Version 2.0 Scaffolding

--- app.py
+++ app.py
@@ -3,9 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+# Route for Home/Index Page
+@app.route('/', methods=["GET"])
 def index():
     return render_template("index.html")
+
+
+# Route for adding a post to the system
+@app.route('/add_post', methods=["GET", "POST"])
+def add_post():
+    return render_template("add_post.html")
+
+# Route for the tutorial/about me page
+@app.route('/about', methods=["GET"])
+def about():
+    return render_template("about.html")
+
+
+
 
 
 if __name__ == '__main__':
