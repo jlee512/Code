b0890fca2986aa42450df42b4f7f0ce5f7e6931f
Julian Lees <julian.j.lees@gmail.com>
2018-02-24 10:34:47 +1300

    Version 3.1 First template version - including Marshall's content

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+import os, json
 
 app = Flask(__name__)
 
@@ -6,36 +7,38 @@ app = Flask(__name__)
 # Route for Home/Index Page
 @app.route('/', methods=["GET"])
 def index():
-    return render_template("index.html")
-
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static/json", "posts.json")
+    data = json.load(open(json_url))
+    return render_template("index.html", data=data)
 
 # Route for the tutorial/about me page
 @app.route('/about', methods=["GET"])
 def about():
     return render_template("about.html")
 
+
 @app.route('/login', methods=["GET", "POST"])
 def login():
     return render_template("login.html")
+
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     return render_template("register.html")
 
+
 # Route for adding a post to the system
 @app.route('/post', methods=["POST"])
 def post():
-
     return render_template("add_post.html")
+
 
 @app.route('/like/<int:post_id>', methods=["GET"])
 def like(post_id):
-
     print(post_id)
 
     return redirect("/")
-
-
 
 
 if __name__ == '__main__':
