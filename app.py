cb1e580ed549dcef0144a736f4a63acdd9809f7d
Julian Lees <julian.j.lees@gmail.com>
2018-02-24 01:10:43 +1300

    Version 2.1 Scaffolding

--- app.py
+++ app.py
@@ -19,6 +19,16 @@ def add_post():
 def about():
     return render_template("about.html")
 
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+
 
 
 
